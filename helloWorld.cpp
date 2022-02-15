7209f2a2dc50575fd9f5da5144b7ad7f2a7ff36b
FatihSafaat <fatihsafaat@gmail.com>
2022-02-15 11:16:00 +0700

    Penambahan Fitur Next

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -3,5 +3,7 @@ using namespace std;
 
 int main()
 {
-    cout <<"Ini adalah penambahan fitur pertama"<<endl;
+    cout <<"Ini adalah penambahan fitur terakhir"<<endl;
+    cout <<"Nama saya Fatih Safaat"<<endl;
+    cout <<"Saya mengikuti program Becomming professional UI/UX Designer";
 }
